17ba421c0d8f456d5f395c7a54fce376e55e7e07
saasa <leo.smelov@>
2016-01-13 11:15:14 +0300

    Lol

---
+++ DanWithLeo/MusicRec.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanWithLeo
+{
+    class MusicRec
+    {
+        string Name;
+        double Leanth;
+        string Author;
+    }
+}
